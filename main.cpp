fe6a4769304a9980918df4f7c1242f510a48c9d2
ebirch <ethanjbirch@gmail.com>
2014-01-26 16:15:08 -0700

    Buddhabrot set generation should be working.  Still need to add list merging and file output

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <random>
 #include <complex>
 
+std::vector<std::complex<double>> getPoints(std::complex<double> point, int maxIter);
+
 int main(int argc, char **argv){
 	int numprocessors, rank, namelen;
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
@@ -13,20 +15,33 @@ int main(int argc, char **argv){
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Get_processor_name(processor_name, &namelen);
 
-	// srand(rank);
+	int maxPoints = 100;
+	int maxIter = 10;
+
 	std::mt19937 random(rank);
 	std::uniform_real_distribution<> realGen(-2.5, 1);
 	std::uniform_real_distribution<> complexGen(-1, 1);
+	std::vector<std::complex<double>> pointList;
 
-	std::complex<double> point(realGen(random), complexGen(random));
-
-	if ( rank == 0 ){
-		std::cout << "Processor name: " << processor_name << "\n";
-		std::cout << "master (" << rank << "/" << numprocessors << ")\n";
-	} 
-	else {
-		std::cout << "slave  (" << rank << "/" << numprocessors << ")\n";
+	for(int i = 0 ; i < maxPoints ; ++i){
+		auto tempList = getPoints(std::complex<double>(realGen(random), complexGen(random)), maxIter);
+		if(tempList.size()){
+			pointList.insert(pointList.end(), tempList.begin(), tempList.end());
+		}
 	}
+
+	std::cout<<"End size: "<<pointList.size()<<std::endl;
+
 	MPI_Finalize();
-	return 0;
+}
+
+std::vector<std::complex<double>> getPoints(std::complex<double> point, int maxIter){
+	std::complex<double> temp(point);
+	std::vector<std::complex<double>> pointsList;
+	int iter = 0;
+	while(temp.real() * temp.real() + temp.imag() * temp.imag() < 4 && iter++ < maxIter){
+		pointsList.push_back(temp);
+		temp = temp * temp + point;
+	}
+	return (iter == maxIter) ? std::vector<std::complex<double>>() : pointsList;
 }
