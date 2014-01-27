4b441e36d2e497e24df1ad353e4171b5c7ea4f5c
ebirch <ethanjbirch@gmail.com>
2014-01-26 18:30:40 -0700

    Now generates PPM images.  Kinda boring though, needs a better coloring function

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 #include <vector>
 #include <random>
 #include <complex>
+#include <algorithm>
+#include <fstream>
 
 std::vector<std::complex<double>> getPoints(std::complex<double> point, int maxIter);
+void printSet(std::pair<std::vector<std::vector<int>>, int> pointList, int depth);
+std::pair<std::vector<std::vector<int>>, int> translateCoords(std::vector<std::complex<double>> &pointList, int xRes, int yRes);
 
 int main(int argc, char **argv){
 	int numprocessors, rank, namelen;
@@ -15,8 +19,11 @@ int main(int argc, char **argv){
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Get_processor_name(processor_name, &namelen);
 
-	int maxPoints = 100;
-	int maxIter = 10;
+	int xRes = 500;
+	int yRes = 500;
+	int maxPoints = 100000;
+	int maxIter = 1000;
+	int depth = 256;
 
 	std::mt19937 random(rank);
 	std::uniform_real_distribution<> realGen(-2.5, 1);
@@ -30,7 +37,10 @@ int main(int argc, char **argv){
 		}
 	}
 
-	std::cout<<"End size: "<<pointList.size()<<std::endl;
+// auto temp=translateCoords(pointList, xRes, yRes);
+// std::cout<<temp.second<<std::endl;
+	// printSet(temp, depth);
+	printSet(translateCoords(pointList, xRes, yRes), depth);
 
 	MPI_Finalize();
 }
@@ -44,4 +54,28 @@ std::vector<std::complex<double>> getPoints(std::complex<double> point, int maxI
 		temp = temp * temp + point;
 	}
 	return (iter == maxIter) ? std::vector<std::complex<double>>() : pointsList;
+}
+
+void printSet(std::pair<std::vector<std::vector<int>>, int> pointList, int depth){
+	std::fstream file("buddhabrot.ppm", std::ios::out);
+	file<<"P3\n"<<pointList.first.size()<<" "<<pointList.first[0].size()<<std::endl<<depth<<std::endl;
+	std::for_each(pointList.first.begin(), pointList.first.end(), 
+		[&](std::vector<int> pointRow){
+			std::for_each(pointRow.begin(), pointRow.end(), 
+				[&](int val){
+					val = (val * (depth - 1)) / pointList.second;
+					file<<std::min(val, depth - 1)<<" "<<std::min(val*val*4/255, depth - 1)<<" "<<std::min(val*val*6/255, depth - 1)<<" ";
+				});
+			file<<std::endl;
+		});
+}
+
+std::pair<std::vector<std::vector<int>>, int> translateCoords(std::vector<std::complex<double>> &pointList, int xRes, int yRes){
+	std::vector<std::vector<int>> newCoords(yRes, std::vector<int>(xRes, 0));
+	int maxHit = 0;
+	std::for_each(pointList.begin(), pointList.end(), 
+		[&](std::complex<double> point){
+			maxHit = std::max(maxHit, ++newCoords[(point.imag() + 2) * yRes / 4][(point.real() + 2) * xRes / 4]);
+		});
+	return std::make_pair(newCoords, maxHit);
 }
